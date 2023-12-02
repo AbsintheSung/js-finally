6107419dc179c102d8c1a440fd611aa8904dd027
AbsintheSung <h90243768@gmail.com>
2023-12-02 11:43:24 +0800

    環境變數測試

--- api/adminAPI.js
+++ api/adminAPI.js
@@ -1,6 +1,6 @@
 import httpInstanceAdmin from '../utils/admin_http';
-let urlPath = 'absinthe'
-
+let urlPath = import.meta.env.VITE_APP_PATH
+// console.log(import.meta.env.VITE_APP_PATH)
 
 export function getOrderAdmin(){
     return httpInstanceAdmin({
